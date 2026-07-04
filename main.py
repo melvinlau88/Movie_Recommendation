9993e3b14f47a25f1980f8eb6ae21d4f6539ce30
Melvin <melvinlau88@outlook.com>
2026-07-04 11:12:53 +1000

    Placed in more filter questions

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# """
-# cd C:\Users\melvi\Downloads\VS_Code\Python\Movie_Recommendation
-# git add .
-# git commit -m ""
-# git push
-# """
+r"""
+cd C:\Users\melvi\Downloads\VS_Code\Python\Movie_Recommendation
+git add .
+git commit -m ""
+git push
+"""
 import os
 import requests
 from dotenv import load_dotenv
@@ -25,6 +25,8 @@ params = {
 }
 
 min_rating = int(input("Enter the minimum rating (0-10): "))
+year_release = input("Enter the year of release (YYYY) or leave blank for any year: ")
+language = input("Enter the language code (e.g., 'en' for English) or leave blank for any language: ")
 
 request = requests.get(url, headers=headers, params=params)
 
@@ -36,16 +38,26 @@ if request.status_code == 200:
 
     filtered_movies = [movie for movie in movies if movie["vote_average"] >= min_rating]
     # Year of release
-    # Language
+    if year_release:
+        filtered_movies = [movie for movie in filtered_movies if movie["release_date"].startswith(year_release)]
+
 
     if filtered_movies:
         print(f"Movies with a rating of {min_rating} or higher:")
         for movie in filtered_movies:
-            print(f"{movie['title']} - Rating: {movie['vote_average']}")
+            print("------------------------------")
+            print(f"{movie['title']}")
+            print(f"Overview: {movie['overview']}")
+            print(f"Release Date: {movie['release_date']}")
+            print(f"Language: {movie['original_language']}")
+            print(f"Rating: {movie['vote_average']}")
     else:
-        print(f"No movies found with a rating of {min_rating} or higher.")
+        print(f"No movies found")
+
+    print("------------------------------")
 
     # When printing, don't just give filter but also additional information
 
 # Potential Features
 # 1. Give URL link to the movie
+# 2. Display images???
